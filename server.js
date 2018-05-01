bf691fb135636dabc25b6fe958ce46c4848bb55d
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-04-30 21:13:26 -0600

    Add server endpoints to get from  and post to projects and palettes tables

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
-
+app.use(bodyParser.json());
 app.use('/', express.static('public'));
+
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select()
+    .then(palettes => response.status(200).json(palettes))
+    .catch(error => response.status(500).json({ error }));
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  const palette = request.body;
+  for (let requiredParam of ['name', 'color-1', 'color-2', 'color-3', 'color-4', 'color-5', 'projectId']) {
+    if (!palette[requiredParam]) {
+      return response.status(422)
+        .send({ error: `Missing required ${requiredParam} property.`});
+    }
+  }
+
+  database('palettes').insert(palette, 'id')
+    .then(palette => response.status(201).json({ id: palette[0] }))
+    .catch(error => response.status(500).json({ error }));
+});
+
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then(projects => response.status(200).json(projects))
+    .catch(error => response.status(500).json({ error }));
+})
+
+app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
+  if (!project.projectName) {
+    return response.status(422)
+      .send({ error: `Missing project name parameter` });
+  }
+  database('projects').insert(project, 'id')
+    .then(project => response.status(201).json({ id: project[0] }))
+    .catch(error => response.status(500).json({ error }));
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Palette Picker server running on ${app.get('port')}.`);
