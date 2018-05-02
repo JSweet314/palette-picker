37c0283bf972a2faf2ccee33ad04e2549da2a968
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-05-02 15:22:50 -0600

    Refactor handleDeletePalette

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,7 @@ const $generatePaletteBtn = $('button#generate-palette');
 const $paletteNameInput = $('.palette-form input');
 const $projects = $('.projects');
 const $selectProject = $('#select-project');
+
 const palettePicker = new PalettePicker();
 
 const setPalette = () => {
@@ -23,7 +24,11 @@ const mapProjectToHTML = async project => {
   const palettes = await palettePicker.palettes
   const matchingPalettes = palettes.filter(palette => palette.projectId === id);
   const paletteHTML = await mapPalettesToHTML(matchingPalettes);
-  return `<article class="project"><h4>${projectName}</h4>${paletteHTML}</article>`;
+  return `
+    <article class="project">
+      <h4>${projectName}</h4>
+      ${paletteHTML}
+    </article>`;
 }
 
 const mapPalettesToHTML = palettes => {
@@ -35,19 +40,29 @@ const mapPalettesToHTML = palettes => {
           <button class="delete-palette" data-id=${palette.id}></button>
         </div>
         <div class="palette">
-          <div class='project-palette-color' style="background-color: ${palette["color-1"]}">
+          <div 
+            class='project-palette-color' 
+            style="background-color: ${palette["color-1"]}">
             <p class="project-palette-title">${palette["color-1"]}</p>
           </div>
-          <div class='project-palette-color' style="background-color: ${palette["color-2"]}">
+          <div 
+            class='project-palette-color' 
+            style="background-color: ${palette["color-2"]}">
             <p class="project-palette-title">${palette["color-2"]}</p>
           </div>
-          <div class='project-palette-color' style="background-color: ${palette["color-3"]}">
+          <div 
+            class='project-palette-color' 
+            style="background-color: ${palette["color-3"]}">
             <p class="project-palette-title">${palette["color-3"]}</p>
           </div>
-          <div class='project-palette-color' style="background-color: ${palette["color-4"]}">
+          <div 
+            class='project-palette-color' 
+            style="background-color: ${palette["color-4"]}">
             <p class="project-palette-title">${palette["color-4"]}</p>
           </div>
-          <div class='project-palette-color' style="background-color: ${palette["color-5"]}">
+          <div 
+            class='project-palette-color' 
+            style="background-color: ${palette["color-5"]}">
             <p class="project-palette-title">${palette["color-5"]}</p>
           </div> 
         </div
@@ -60,10 +75,14 @@ const setProjects = async () => {
   $('option.project-name').remove();
   const projects = await palettePicker.projects;
   projects.forEach(async project => {
-    const {projectName, id} = project
+    const { projectName, id } = project
     const projectHTML = await mapProjectToHTML(project);
     $projects.append(projectHTML);
-    $selectProject.append(`<option class="project-name" value="${projectName}" data-key=${id}>${projectName}</option>`);
+    $selectProject.append(
+      `<option class="project-name" value="${projectName}" data-key=${id}>
+        ${projectName}
+      </option>`
+    );
   });
 }
 
@@ -83,42 +102,39 @@ const handleLockClick = event => {
   palettePicker.currentPalette.toggleColorLock(key);
 }
 
-const initializeApp = () => {
-  setPalette();
-  setProjects();
-};
+const postNewPalette = async (name, projectName) => {
+  const projects = await palettePicker.projects;
+  const project = await projects.find(project =>
+    project.projectName === projectName)
+  const projectId = project.id;
+  const colors = palettePicker.packageColorsForProject();
+  const palette = { ...colors, projectId, name };
+  const validation = await palettePicker.postPalette(palette);
+  if (!validation) {
+    $('.palette-error').text('Something went wrong, please try again');
+  } else {
+    $('.palette-error').text('');
+    await setProjects();
+  }
+}
 
-const handleSavePalette = async event => {
+const handleSavePalette =  event => {
   event.preventDefault();
   const name = $paletteNameInput.val();
   const projectName = $selectProject.val();
-  console.log(projectName)
-  if (!projectName) {
-    $('.palette-error').text('You must select/create a project');
+  if (!projectName || !name) {
+    $('.palette-error').text('You must pick a project and name your palette');
     return;
   }
-  const projects = await palettePicker.projects;
-  const project = await projects.find(project => project.projectName === projectName)
-  const projectId = project.id;
-  const colors = palettePicker.currentPalette.colors.map(color => color.hex)
-    .reduce((palette, color, index) => {
-      palette[`color-${index + 1}`] = color;
-      return palette;
-    }, {});
-    const palette = {...colors, projectId, name};
-    const validation = await palettePicker.postPalette(palette);
-    if (!validation) {
-      $('.palette-error').text('Something went wrong, please try again');
-    }
-    $('.palette-error').text('');
-    await setProjects();
+  postNewPalette(name, projectName);
 }
 
 const handleSaveProject = async event => {
   event.preventDefault();
   const name = $('#project-name').val();
   const projects = await palettePicker.projects;
-  const alreadyExists = projects.some(project => project.projectName.toLowerCase() === name.toLowerCase());
+  const alreadyExists = projects.some(project =>
+    project.projectName.toLowerCase() === name.toLowerCase());
   if (!alreadyExists) {
     const response = await palettePicker.postProject({ projectName: name });
     $('.project-error').text('');
@@ -131,6 +147,17 @@ const handleSaveProject = async event => {
   }
 }
 
+const handleDeletePalette = async event => {
+  const id = event.target.dataset;
+  await palettePicker.deletePalette(id);
+  await setProjects();
+}
+
+const initializeApp = () => {
+  setPalette();
+  setProjects();
+};
+
 $(window).on('load', initializeApp);
 
 $generatePaletteBtn.on('click', updateCurrentPalette);
@@ -141,9 +168,4 @@ $savePaletteBtn.on('click', handleSavePalette);
 
 $saveProjectBtn.on('click', handleSaveProject);
 
-$projects.on('click', '.delete-palette', async event => {
-  const id = event.target.dataset;
-  await palettePicker.deletePalette(id);
-  await setProjects();
-  
-});
+$projects.on('click', '.delete-palette', handleDeletePalette);
