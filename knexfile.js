cfc49828c3464205af63c4e3414fcc7aca5e580d
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-05-01 21:41:54 -0600

    Add procfile and heroku config

--- knexfile.js
+++ knexfile.js
@@ -8,5 +8,13 @@ module.exports = {
       directory: './db/migrations'
     },
     userNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
